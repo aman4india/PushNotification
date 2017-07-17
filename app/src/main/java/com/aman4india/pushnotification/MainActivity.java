23a53e258b47f7438f8ebf6240f3891da08ff2d4
Aman <iamamansingh55@gmail.com>
2017-07-17 16:21:53 +0530

    Initial commit

---
+++ app/src/main/java/com/aman4india/pushnotification/MainActivity.java
@@ -0,0 +1,21 @@
+package com.aman4india.pushnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://console.firebase.google.com");
+
+    }
+}
